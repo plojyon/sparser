58327b5f40ef6d34a79e439f1f44a20d8da2a1cc
Yon Ploj <yon.ploj@gmail.com>
2022-02-25 23:15:21 +0100

    Fix blacklist generating

--- find_invalid_products.py
+++ find_invalid_products.py
@@ -19,9 +19,9 @@ quantities = ["sugar", "carbohydrates", "fat", "saturated_fat", "protein", "salt
 for item in products:
     for quantity in quantities:
         if item.has_value(quantity):
-            if getattr(item, quantity) > item.weight:
+            if getattr(item, quantity) > 100:
                 print(
-                    "{id}: {name} ima {value}g {quantity}, skupno {weight:.0f}g".format(
+                    "{id}: {name} ima {value}g {quantity} na 100g produkta".format(
                         id=item.id,
                         name=item.name,
                         value=getattr(item, quantity),
@@ -29,3 +29,21 @@ for item in products:
                         weight=item.weight,
                     )
                 )
+    if item.has_values(["fat", "saturated_fat"]) and item.fat < item.saturated_fat:
+        print(
+            "{id}: {name} ima {fat}g mascob, od tega {saturated}g nasicenih".format(
+                id=item.id,
+                name=item.name,
+                fat=item.fat,
+                saturated=item.saturated_fat,
+            )
+        )
+    if item.has_values(["carbohydrates", "sugar"]) and item.carbohydrates < item.sugar:
+        print(
+            "{id}: {name} ima {ch}g ogljikovih hidratov, od tega {sugar}g sladkorjev".format(
+                id=item.id,
+                name=item.name,
+                ch=item.carbohydrates,
+                sugar=item.sugar,
+            )
+        )
---
+++ napake.py
@@ -0,0 +1,29 @@
+import sparser
+
+all_products = sparser.data(use_blacklist=False)
+
+# filter products with a defined neto weight
+products = list(filter(lambda x: x.has_value("weight"), all_products))
+
+nutri_slo = {
+    "sugar": "sladkorjev",
+    "carbohydrates": "ogljikovih hidratov",
+    "saturated_fat": "nasicenih mascob",
+    "protein": "beljakovin",
+    "fat": "mascob",
+    "salt": "soli",
+}
+
+quantities = ["sugar", "carbohydrates", "fat", "saturated_fat", "protein", "salt"]
+invalid = []
+for item in products:
+    for quantity in quantities:
+        if item.has_value(quantity):
+            if getattr(item, quantity) > 100:
+                invalid.append(item.id)
+    if item.has_values(["fat", "saturated_fat"]) and item.fat < item.saturated_fat:
+        invalid.append(item.id)
+    if item.has_values(["carbohydrates", "sugar"]) and item.carbohydrates < item.sugar:
+        invalid.append(item.id)
+
+print(list(set(invalid)))
